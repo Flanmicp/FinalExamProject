ac334d1dad23f98b862b62e07d16e3acbbb71cb5
Michael Flanagan <Michael Flanagan@THEDEATHSTAR>
2018-05-19 22:06:19 -0400

    :)

--- FinalExam/src/main/java/pkgApp/controller/RetirementController.java
+++ FinalExam/src/main/java/pkgApp/controller/RetirementController.java
@@ -65,11 +65,16 @@ public class RetirementController implements Initializable {
 		// "\\d*?" - means any decimal number
 		// "\\d*(\\.\\d*)?" means any decimal, then optionally a period (.), then
 		// decmial
-		hmTextFieldRegEx.put(txtYearsToWork, "\\d*?");
-		hmTextFieldRegEx.put(txtAnnualReturnWorking, "\\d*(\\.\\d*)?");
+		hmTextFieldRegEx.put(txtYearsToWork, "([0-9]|[1-3][0-9]|40)");
+		hmTextFieldRegEx.put(txtAnnualReturnWorking, "(0|0\\.[0-1]|0\\.[0][0-9]|\\.[0-1]|\\.[0][0-9])");
+		hmTextFieldRegEx.put(txtYearsRetired, "([0-9]|[1][0-9]|20)");
+		hmTextFieldRegEx.put(txtAnnualReturnRetired, "(0|0\\.[0-1]|0\\.[0][0-9]|\\.[0-1]|\\.[0][0-9])");
+		hmTextFieldRegEx.put(txtRequiredIncome, "(264[2-9]|26[5-9][0-9]|2[7-9][0-9]{2}|[3-9][0-9]{3}|10000)");
+		hmTextFieldRegEx.put(txtMonthlySSI, "([0-9]|[1-8][0-9]|9[0-9]|[1-8][0-9]{2}|9[0-8][0-9]|99[0-9]|1[0-9]{3}|2[0-5][0-9]{2}|26[0-3][0-9]|264[0-2])");
+		
 
 		// Check out these pages (how to validate controls):
-		// https://stackoverflow.com/questions/30935279/javafx-input-validation-textfield
+		// https://stackoverflow.com/questions/30935279/javafx-nput-validation-textfield
 		// https://stackoverflow.com/questions/40485521/javafx-textfield-validation-decimal-value?rq=1
 		// https://stackoverflow.com/questions/8381374/how-to-implement-a-numberfield-in-javafx-2-0
 		// There are some examples on how to validate / check format
@@ -97,11 +102,11 @@ public class RetirementController implements Initializable {
 		}
 
 		//
-		// TODO: Validate Working Annual Return %, accept only numbers and decimals
-		// TODO: Validate Years retired, accepted only decimals
-		// TODO: Validate Retired Annual Return %, accept only numbers and deciamls
-		// TODO: Validate Required Income, accept only decimals
-		// TODO: Validate Monthly SSI, accept only decimals
+		// DONE: Validate Working Annual Return %, accept only numbers and decimals
+		// DONE: Validate Years retired, accepted only decimals
+		// DONE: Validate Retired Annual Return %, accept only numbers and deciamls
+		// DONE: Validate Required Income, accept only decimals
+		// DONE: Validate Monthly SSI, accept only decimals
 	}
 
 	@FXML
@@ -115,21 +120,44 @@ public class RetirementController implements Initializable {
 		// Clear, enable txtYearsToWork
 		txtYearsToWork.clear();
 		txtYearsToWork.setDisable(false);
-
-		// TODO: Clear, enable the rest of the input controls. Hint! You already have a
+		
+		txtAnnualReturnWorking.clear();
+		txtAnnualReturnWorking.setDisable(false);
+		
+		txtYearsRetired.clear();
+		txtYearsRetired.setDisable(false);
+		
+		txtAnnualReturnRetired.clear();
+		txtAnnualReturnRetired.setDisable(false);
+		
+		txtRequiredIncome.clear();
+		txtRequiredIncome.setDisable(false);
+		
+		txtMonthlySSI.clear();
+		txtMonthlySSI.setDisable(false);
+	
+		// DONE: Clear, enable the rest of the input controls. Hint! You already have a
 		// HashMap of all the input controls....!!!!
 	}
 
 	@FXML
 	public void btnCalculate() {
 
-		System.out.println("calculating");
+	System.out.println("calculating");
 
-		txtSaveEachMonth.setDisable(false);
-		txtWhatYouNeedToSave.setDisable(false);
+	txtSaveEachMonth.setDisable(false);
+	txtWhatYouNeedToSave.setDisable(false);
 
-		// TODO: Calculate txtWhatYouNeedToSave value...
-		// TODO: Then calculate txtSaveEachMonth, using amount from txtWhatYouNeedToSave
-		// as input
+	// DONE: Calculate txtWhatYouNeedToSave value...
+	// DONE: Then calculate txtSaveEachMonth, using amount from txtWhatYouNeedToSave
+	// as input
+	Retirement ret = new Retirement(Integer.parseInt(txtYearsToWork.getText()), Double.parseDouble(txtAnnualReturnWorking.getText()), Integer.parseInt(txtYearsRetired.getText()),
+	Double.parseDouble(txtAnnualReturnRetired.getText()), Double.parseDouble(txtRequiredIncome.getText()), Double.parseDouble(txtMonthlySSI.getText()));
+
+	txtWhatYouNeedToSave.setText(Double.toString(ret.TotalAmountToSave()));
+	txtSaveEachMonth.setText(Double.toString(ret.MonthlySavings()));
+
+	txtSaveEachMonth.setDisable(true);
+	txtWhatYouNeedToSave.setDisable(true);
 	}
 }
